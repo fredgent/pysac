2e521900258a2b4cad18d3ad3400a1a5a1a67d31
Frederick Gent <fred.gent.ncl@gmail.com>
2016-05-17 14:33:47 +0100

    Revised to include parallel write of data

--- examples/mhs_atmosphere/val_mtw_atmosphere.py
+++ examples/mhs_atmosphere/val_mtw_atmosphere.py
@@ -102,17 +102,21 @@ ax, ay, az = np.mgrid[coords['xmin']:coords['xmax']:1j*model_pars['Nxyz'][0],
                       coords['ymin']:coords['ymax']:1j*model_pars['Nxyz'][1],
                       coords['zmin']:coords['zmax']:1j*model_pars['Nxyz'][2]]
 
+axindex=np.arange(model_pars['Nxyz'][0])
 # split the grid between processes for mpi
 if l_mpi:
     x_chunks = np.array_split(ax, size, axis=0)
     y_chunks = np.array_split(ay, size, axis=0)
     z_chunks = np.array_split(az, size, axis=0)
+    i_chunks = np.array_split(axindex, size, axis=0)
 
     x = comm.scatter(x_chunks, root=0)
     y = comm.scatter(y_chunks, root=0)
     z = comm.scatter(z_chunks, root=0)
+    xindex=i_chunks[rank]
 else:
     x, y, z = ax, ay, az
+    xindex=axindex
 
 x = u.Quantity(x, unit=coords['xmin'].unit)
 y = u.Quantity(y, unit=coords['ymin'].unit)
@@ -205,158 +209,82 @@ if not option_pars['l_hdonly']:
 else:
     pbeta  = magp+1.0    #dummy to avoid NaN
 alfven = np.sqrt(2.*magp/rho)
-#if rank == 0:
-#    print'Alfven speed Z.min to Z.max =',\
-#    alfven[model_pars['Nxyz'][0]/2,model_pars['Nxyz'][1]/2, 0].decompose(),\
-#    alfven[model_pars['Nxyz'][0]/2,model_pars['Nxyz'][1]/2,-1].decompose()
 cspeed = np.sqrt(physical_constants['gamma']*pressure/rho)
-# collect variables under MPI
-if option_pars['l_mpi']:
-    gather_vars = [
-                   rho.si.value,
-                   Bx.si.value,
-                   By.si.value,
-                   Bz.si.value,
-                   energy.si.value
-                  ]
-    unit_vars = [
-                   rho.unit.to_string(),
-                   Bx.unit.to_string(),
-                   By.unit.to_string(),
-                   Bz.unit.to_string(),
-                   energy.unit.to_string()
-                  ]
-    concat_vars = []
-    for var in gather_vars:
-        concat_vars.append(comm.gather(var, root=0))
-    if rank == 0:
-        out_vars = []
-        for i in range(0,len(unit_vars)):
-            out_vars.append(u.Quantity(np.concatenate(concat_vars[i], axis=0),
-            unit=u.Unit(unit_vars[i])))
-        rho,Bx,By,Bz,energy = out_vars
-        del out_vars
-    del gather_vars, concat_vars, unit_vars
-    gather_vars = [
-                   Fx.si.value,Fy.si.value
-                  ]
-    unit_vars = [Fx.unit.to_string(), Fy.unit.to_string()]
-    concat_vars = []
-    for var in gather_vars:
-        concat_vars.append(comm.gather(var, root=0))
-
-    if rank == 0:
-        out_vars = []
-        for i in range(0,len(unit_vars)):
-            out_vars.append(u.Quantity(np.concatenate(concat_vars[i], axis=0),
-            unit=u.Unit(unit_vars[i])))
-
-        Fx,Fy = out_vars
-        del out_vars
-    del gather_vars, concat_vars
-    gather_vars = [
-                   pressure_m.si.value,
-                   rho_m.si.value,
-                   temperature.si.value,
-                   pbeta.si.value,
-                   alfven.si.value,
-                   cspeed.si.value,
-                   Btensx.si.value,
-                   Btensy.si.value
-                  ]
-    unit_vars = [
-                   pressure_m.unit.to_string(),
-                   rho_m.unit.to_string(),
-                   temperature.unit.to_string(),
-                   pbeta.unit.to_string(),
-                   alfven.unit.to_string(),
-                   cspeed.unit.to_string(),
-                   Btensx.unit.to_string(),
-                   Btensy.unit.to_string()
-                  ]
-    concat_vars = []
-    for var in gather_vars:
-        concat_vars.append(comm.gather(var, root=0))
-
-    if rank == 0:
-        out_vars = []
-        for i in range(0,len(unit_vars)):
-            out_vars.append(u.Quantity(np.concatenate(concat_vars[i], axis=0),
-            unit=u.Unit(unit_vars[i])))
-
-        pressure_m, rho_m, temperature, pbeta,\
-            alfven, cspeed, Btensx, Btensy = out_vars
-        del out_vars
-    del gather_vars, concat_vars
-
 #============================================================================
 # Save data for SAC and plotting
 #============================================================================
 # set up data directory and file names
 # may be worthwhile locating on /data if files are large
-if rank == 0:
-    datadir = os.path.expanduser('~/Documents/mhs_atmosphere/'+model_pars['model']+'/')
-    filename = datadir + model_pars['model'] + option_pars['suffix']
-    if not os.path.exists(datadir):
-        os.makedirs(datadir)
-    sourcefile = datadir + model_pars['model'] + '_sources' + option_pars['suffix']
-    aux3D = datadir + model_pars['model'] + '_3Daux' + option_pars['suffix']
-    aux1D = datadir + model_pars['model'] + '_1Daux' + option_pars['suffix']
-    # save the variables for the initialisation of a SAC simulation
-    atm.mhs_snapshot.save_SACvariables(
-              filename,
-              rho,
-              Bx,
-              By,
-              Bz,
-              energy,
-              option_pars,
-              physical_constants,
-              coords,
-              rank,
-              model_pars['Nxyz']
-             )
+datadir = os.path.expanduser('~/Documents/mhs_atmosphere/'+model_pars['model']+'/')
+filename = datadir + model_pars['model'] + option_pars['suffix']
+if not os.path.exists(datadir):
+    os.makedirs(datadir)
+sourcefile = datadir + model_pars['model'] + '_sources' + option_pars['suffix']
+aux3D = datadir + model_pars['model'] + '_3Daux' + option_pars['suffix']
+aux1D = datadir + model_pars['model'] + '_1Daux' + option_pars['suffix']
+# save the variables for the initialisation of a SAC simulation
+atm.mhs_snapshot.save_SACvariables(
+          filename,
+          rho,
+          Bx,
+          By,
+          Bz,
+          energy,
+          option_pars,
+          physical_constants,
+          coords,
+          model_pars['Nxyz'],
+          xindex,
+          rank=rank,
+          collective=collective
+         )
 # save the balancing forces as the background source terms for SAC simulation
-    atm.mhs_snapshot.save_SACsources(
-              sourcefile,
-              Fx,
-              Fy,
-              option_pars,
-              physical_constants,
-              coords,
-              rank,
-              model_pars['Nxyz']
-             )
+atm.mhs_snapshot.save_SACsources(
+          sourcefile,
+          Fx,
+          Fy,
+          option_pars,
+          physical_constants,
+          coords,
+          model_pars['Nxyz'],
+          xindex,
+          rank=rank,
+          collective=collective
+         )
 # save auxilliary variable and 1D profiles for plotting and analysis
-    atm.mhs_snapshot.save_auxilliary3D(
-              aux3D,
-              pressure_m,
-              rho_m,
-              temperature,
-              pbeta,
-              alfven,
-              cspeed,
-              Btensx,
-              Btensy,
-              option_pars,
-              physical_constants,
-              coords,
-              rank,
-              model_pars['Nxyz']
-             )
+atm.mhs_snapshot.save_auxilliary3D(
+          aux3D,
+          pressure_m,
+          rho_m,
+          temperature,
+          pbeta,
+          alfven,
+          cspeed,
+          Btensx,
+          Btensy,
+          option_pars,
+          physical_constants,
+          coords,
+          model_pars['Nxyz'],
+          xindex,
+          rank=rank,
+          collective=collective
+         )
+if rank==0:
     atm.mhs_snapshot.save_auxilliary1D(
-              aux1D,
-              pressure_Z,
-              rho_Z,
-              Rgas_Z,
-              option_pars,
-              physical_constants,
-              coords,
-              rank,
-              model_pars['Nxyz']
-             )
+          aux1D,
+          pressure_Z,
+          rho_Z,
+          Rgas_Z,
+          option_pars,
+          physical_constants,
+          coords,
+          model_pars['Nxyz'],
+          rank=rank,
+          collective=False
+         )
 if rho.min()<0 or pressure.min()<0:
-    print"FAIL: negative rho.min() {} and/or pressure.min() {}.".format(
-    rho.min(),pressure.min())
+    print"FAIL rank {}: negative rho.min() {} and/or pressure.min() {}.".format(
+                            rank,rho.min(),pressure.min())
 FWHM = 2*np.sqrt(np.log(2))*model_pars['radial_scale']
 print'FWHM(0) =',FWHM
